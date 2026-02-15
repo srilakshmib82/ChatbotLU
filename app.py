abf2fc0c3039b8b97df757b9e175fb36235c3f18
Hema <hemavenkatasrilakshmibadduri@gmail.com>
2026-02-15 20:13:36 +0530

    updated app.py for deployment

--- app.py
+++ app.py
@@ -22,5 +22,8 @@ def chat():
         "pos_tags": pos_tags
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
